0adf7c5677cd51e8777262f7c8cd42b6fd4294bc
Bogdan <bbs30042003@gmail.com>
2022-06-21 00:03:44 +0300

    commit

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -9,14 +9,15 @@ import { setCurrentPage } from "../../store";
 import { createPages } from "../../pagesCreator/pagesCreator";
 
 const Main = () => {
-    const loading = useSelector(loadingData);
-    const error = useSelector(errorData);
     const repos = useSelector(reposData);
     const currentPage = useSelector(currentPageData);
     const totalCount = useSelector(totalCountData);
+    const loading = useSelector(loadingData);
+    const error =useSelector(errorData);
     const perPage = useSelector(perPageData); 
     const dispatch = useDispatch();
     const [value, setValue] = useState('');
+
     const pages = [];
     const pagesCount = Math.ceil( totalCount/perPage );
 
@@ -24,7 +25,7 @@ const Main = () => {
 
     useEffect (() => {
         dispatch(fetchRepos(value, currentPage, perPage));
-    }, [currentPage, value]);
+    }, [currentPage, value, perPage]);
 
     const handleChange = (e) => {
         dispatch(setCurrentPage(1));
@@ -34,21 +35,27 @@ const Main = () => {
     const reposFilter = repos.filter((reposinfo) => {
         return reposinfo.name.toLowerCase().includes(value.toLowerCase())
     });
-
-    // if (loading) {  
-    //     return () 
-    // } 
-    // if (error) {
-    //     return () 
-    // }
-
-    return (<>
+    
+    if (loading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    } else if (error) {
+        return (
+            <div>
+                Error +-+
+            </div>
+        )
+    } else {
+        return (<>
             <section className="searchSection">
                 <div className="Input">
                     <input type="text" id="input" className="Input-text" placeholder="Search" onChange={handleChange} autoComplete='off'></input>
                 </div>
             </section>
-
+    
             <section className="reposSection">
                 <div className="reposCardBox">
                     {reposFilter.map((item) => (  
@@ -57,17 +64,20 @@ const Main = () => {
                                 link={item.html_url} 
                                 commitdate={item.updated_at} 
                                 stars={item.stargazers_count} 
-                                reposname={item.name} key={item.id}>
+                                reposname={item.name} 
+                                key={item.id}>
                             </ReposItem>
                     ))}
                 </div>
             </section>
-
+    
             <section className="paginatorBox">
                 <div className="paginator">
                     {pages.map((page, index) => <span key={index} className={currentPage === page ? "current-page" : "page"} onClick={() => dispatch(setCurrentPage(page))}>{page}</span>)}
                 </div>
-            </section></>)
+            </section></>
+        )
+    }
 }
 
 export default Main; 
